2431e397c84538302b46b70a0f097776e487dc66
Dominik Neukamm <domneuk@gmail.com>
2021-03-28 04:49:57 +0200

    Creating HomePage and using it in App

---
+++ src/components/PageHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Hello page home</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json';
+
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+    };
+  },
+};
+</script>
+
+<style scoped>
+body {
+  color: 999;
+}
+</style>
