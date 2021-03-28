0e1a37ce6efb9da57d086679a2280dd9164dd117
Dominik Neukamm <domneuk@gmail.com>
2021-03-28 13:14:05 +0200

    Adding a NotFound route and component

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="col-full">
+    <h1>Not found</h1>
+    <router-link :to="{ name: 'Home' }">Read some cool threads</router-link>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PageHome from '@/components/PageHome.vue';
 import PageThreadShow from '@/components/PageThreadShow.vue';
+import PageNotFound from '@/components/PageNotFound.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound,
   },
 ];
 
