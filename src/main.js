522db5bfd021c7c3225451f14abafd44491ef1ba
Dominik Neukamm <domneuk@gmail.com>
2021-03-28 03:32:45 +0200

    Intro to app structuring

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const forumApp = createApp(App);
+
+// adding simple component
+// forumApp.component(name: 'Button', component: {});
+
+// using plugins
+// forumApp.use(SomePlugin);
+
+forumApp.mount('#app');
+
+// You can declare multiple apps in  one project
+/*
+const anotherApp = createApp(App);
+anotherApp.use(AnotherPlugin);
+anotherApp.mount(rootContainer: '#anotherapp');
+*/
