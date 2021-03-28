80fc2fadd08b8eee5af9a7e0fdc70818465a45b8
Dominik Neukamm <domneuk@gmail.com>
2021-03-28 06:18:52 +0200

    Updating eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    '@vue/airbnb'
+    '@vue/airbnb',
+    'airbnb',
+    'prettier'
   ],
+  plugins: ['prettier'],
   root: true,
   env: {
     node: true
